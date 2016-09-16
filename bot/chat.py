f9425386bd55dbfe5b70bfcd1f219fa3be0b4c90
olgaklimenko <oklimenko92@mail.ru>
2016-09-16 16:20:23 +0700

    add send hello on chat message

--- bot/chat.py
+++ bot/chat.py
@@ -8,7 +8,5 @@ class Chat(telepot.aio.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
         super(Chat, self).__init__(*args, **kwargs)
 
-    async def on_chat_message(self, msg, **kwargs):
-        if not kwargs.get('parse_mode'):
-            kwargs['parse_mode'] = 'HTML'
-        await self.sender.sendMessage('<h1>Hello</h1>', **kwargs)
+    async def on_chat_message(self, msg):
+        await self.sender.sendMessage('[Hello](http://google.com/)', parse_mode='Markdown')
